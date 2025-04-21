f22d52767d1bf38ae96489830fa29ab7fc2316fa
Augustus-Caesar886 <williambruce398@gmail.com>
2025-04-20 22:31:14 -0700

    Implement Account base class

--- test/AccountTests.cpp
+++ test/AccountTests.cpp
@@ -6,6 +6,14 @@
 using ::testing::_;
 using ::testing::InSequence;
 
+class MockAccount : public Account {
+
+};
+
 TEST(AccountTests, testAccount) {
-    
+    Account user;
+    user = MockAccount::builder().username("W").passkey("123").build();
+    EXPECT_TRUE(user.authenticate("W", "123"));
+    EXPECT_FALSE(user.authenticate("V", "123"));
+    EXPECT_FALSE(user.authenticate("W", "456"));
 }
