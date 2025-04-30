cb8ff0a6f9bfe86049af743e12e7324ffe1c50f7
Augustus-Caesar886 <williambruce398@gmail.com>
2025-04-30 12:47:46 -0700

    Untested implementation of quiz

--- header/Quiz.h
+++ header/Quiz.h
@@ -11,16 +11,41 @@ using std::vector;
 using std::ostream;
 
 struct Answer {
+    string text;
     int mechScore, electricalScore, civilScore, csScore, chemScore; //0-5 scores
 };
 
 class Question {
-    private:
-        string _prompt;
-        Answer answers[5];
     public:
-        //TODO: builder
-
+        static const int possAnswers = 5;
+        string prompt;
+        using collectionType = vector<Answer>;
+        collectionType answers;
+    public:
+        class QuestionBuilder {
+            private:
+                string _prompt;
+                collectionType _answers;
+                using answerNumType = int;
+                answerNumType answerNum = 0;
+            public:
+                QuestionBuilder& prompt(const string& prompt) {
+                    _prompt = prompt;
+                    return *this;
+                };
+                QuestionBuilder& addAnswer(const Answer& ans) {
+                    _answers.push_back( ans);
+                    return *this;
+                }
+                Question build() {
+                    Question q;
+                    q.prompt = _prompt;
+                    q.answers = _answers;
+                    return q;
+                }
+        };
+        QuestionBuilder builder()  { return QuestionBuilder(); }
+        friend class QuestionBuilder;
 };
 
 class Quiz {
@@ -30,9 +55,9 @@ class Quiz {
         string quizName;
         using collectionType = vector<Question>;
         collectionType questionBank;
-        using questionNum = int;
+        using questionNumType = int;
     public:
-        void displayQuestion(ostream&) const;
+        void displayQuestion(questionNumType, ostream&) const;
         void displayResults(ostream&) const;
         void refresh();
 };
--- src/Quiz.cpp
+++ src/Quiz.cpp
@@ -1,1 +1,24 @@
-#include "../header/quiz.h"
+#include "../header/Quiz.h"
+
+using std::endl;
+
+void Quiz::displayQuestion(Quiz::questionNumType questionNum, ostream& out) const {
+    out << questionBank.at(questionNum-1).prompt << endl;
+    char answerOption = 'a';
+    for(int i = 0;i < Question::possAnswers; ++i) {
+        out << (answerOption++) << ". " << questionBank.at(questionNum-1).answers[i].text << endl;
+    }
+}
+void Quiz::displayResults(ostream& out) const {
+    out << "Suggested major: " << recommendedMajor << endl;
+    out << "Scores:" << endl;
+    out << "\tMechanical Engineering: " << mechScore << endl; //TODO: Out of max possible points
+    out << "\tElectrical Engineering: " << electricalScore << endl;
+    out << "\tCivil Engineering: " << civilScore << endl;
+    out << "\tComputer Science: " << csScore << endl;
+    out << "\tChemical Engineering: " << chemScore << endl;
+}
+
+void Quiz::refresh() {
+    //TODO: implement
+}
