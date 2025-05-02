d0a7c0118043b700e7f8b805be5d18803141083d
root <root@LegionGamingPC.>
2025-05-02 14:31:06 -0700

    Fixed hashes so ONLY the hashes are stored. Account storage and load functionality included

--- test/AccountTests.cpp
+++ test/AccountTests.cpp
@@ -12,9 +12,19 @@ class MockAccount : public Account {
 
 TEST(AccountTests, testAccount) {
     Account user = MockAccount::builder().username("W").passkey("123").build();
-    EXPECT_TRUE(user.authenticate("W", "202cb962ac59075b964b07152d234b70"));
+    EXPECT_TRUE(user.authenticate("W", "123"));
     EXPECT_FALSE(user.authenticate("V", "123"));
     EXPECT_FALSE(user.authenticate("W", "456"));
     EXPECT_FALSE(user.authenticate("WW", "123"));
     EXPECT_FALSE(user.authenticate("W", "1234"));
+}
+
+TEST(GetterTests, testGetters) {
+    Account test = MockAccount::builder().username("testusername").passkey("testpasskey").build();
+    EXPECT_TRUE(test.getPasskey() == "c248e14a2f62fd7d99337da21e59d586");
+    EXPECT_FALSE(test.getPasskey() == "testpasskey");
+    EXPECT_TRUE(test.getUsername() == "testusername");
+    EXPECT_FALSE(test.getUsername() == "testtusername");
+
+    // Hashed passkey: c248e14a2f62fd7d99337da21e59d586
 }
