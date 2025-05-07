3688dd16f598fbe87a5067430e638dc24e7d9491
Arleen Kaur <arleenkaurarora9@gmail.com>
2025-05-07 15:13:13 -0700

    Fixed incomplete frontent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';import LoginPage from './loginPage';
+import StudentMenuPage from './studentMenuPage';
+import TeacherMenuPage from './teacherMenuPage';
+import QuizMenuPage from './quizMenuPage';
+import QuizResultPage from './quizResultPage';
 
 function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/student" element={<StudentMenuPage />} />
+        <Route path="/teacher" element={<TeacherMenuPage />} />
+        <Route path="/quiz" element={<QuizMenuPage />} />
+        <Route path="/result" element={<QuizResultPage />} />
+      </Routes>
+    </Router>
   );
 }
 
