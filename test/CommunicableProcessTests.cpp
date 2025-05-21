07bed341199ce09a988f98b20fed9fe7c5f54454
William Bruce <73004193+Augustus-Caesar886@users.noreply.github.com>
2025-05-20 23:05:10 -0700

    Alter test for linux environment

--- test/CommunicableProcessTests.cpp
+++ test/CommunicableProcessTests.cpp
@@ -9,6 +9,6 @@ using ::testing::InSequence;
 #include <filesystem>
 
 TEST(CommunicableProcessTests, testProcess) {
-    CommunicableProcess python3("/Library/Frameworks/Python.framework/Versions/3.12/bin/python3", "python3");
+    CommunicableProcess python3("/usr/bin/python3", "python3");
     EXPECT_EQ(python3.query("FRQLLM.py I enjoy computing"), "Computer Science");
 }
